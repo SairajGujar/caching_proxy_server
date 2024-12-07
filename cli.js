7060ebaa29666609a6d6825cb1392398ee9675e3
SAiii0101 <sairajgujar2000@gmail.com>
2024-12-07 18:06:28 +0530

    final commit

--- cli.js
+++ cli.js
@@ -1,8 +1,9 @@
 import { Command } from "commander";
 import axios from "axios";
 import express from "express";
-import NodeCache from "node-cache";
-const myCache = new NodeCache();
+import Redis from "ioredis";
+
+const redis = new Redis();
 const program = new Command();
 
 program
@@ -11,6 +12,7 @@ program
   .version("1.0.0");
 
 program
+  .command("start")
   .requiredOption("-p, --port <number>", "Port number")
   .requiredOption("-u, --url <string>", "Base URL")
   .action(({ port, url }) => {
@@ -24,31 +26,43 @@ program
       process.exit(1);
     }
     const app = express();
-    async function handleRequest(req, res){
-      const newurl = `${url.replace(/\/+$/, '')}/${req.originalUrl.replace(/^\/+/, '')}`;
-      console.log(`Forwarding request to: ${newurl}`); 
-      const cachedResponse = myCache.get(newurl);
 
-      if(cachedResponse){
-          res.setHeader('X-Cache', 'HIT');
-          return res.status(200).send(cachedResponse.data);
+    async function handleRequest(req, res) {
+      const newurl = `${url.replace(/\/+$/, "")}/${req.originalUrl.replace(/^\/+/, "")}`;
+      console.log(`Forwarding request to: ${newurl}`);
+
+      const cachedResponse = await redis.get(newurl);
+
+      if (cachedResponse) {
+        res.setHeader("X-Cache", "HIT");
+        return res.status(200).send(JSON.parse(cachedResponse));
       }
 
       try {
-          const response = await axios.get(newurl);
-          const responseData = response.data; 
-          myCache.set(newurl, responseData);
-          res.setHeader('X-Cache', 'MISS');
-          res.status(response.status).send(responseData);
-        } catch (error) {
-          console.error(`Request failed with status code: ${error.response ? error.response.status : 500}`);
-          res.status(error.response ? error.response.status : 500).send(error.message);
-        }
+        const response = await axios.get(newurl);
+        const responseData = response.data;
+
+        await redis.set(newurl, JSON.stringify(responseData), "EX", 3600);
+        res.setHeader("X-Cache", "MISS");
+        res.status(response.status).send(responseData);
+      } catch (error) {
+        console.error(`Request failed with status code: ${error.response ? error.response.status : 500}`);
+        res.status(error.response ? error.response.status : 500).send(error.message);
       }
-      app.get('*', handleRequest);
+    }
+
+    app.get("*", handleRequest);
+
     app.listen(port, () => {
-      console.log(`listening on ${port}`);
+      console.log(`Listening on port ${port}`);
     });
+  });
+
+program
+  .command("clear")
+  .action(async () => {
+    await redis.flushall();
+    console.log("Cache cleared");
   });
 
 program.parse(process.argv);
